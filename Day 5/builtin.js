ad728dd76e9761768e050f6d6d5e8d5e8c4ecf26
Anish Pandit <anish1924@hotmail.com>
2023-10-26 23:08:57 +0200

    uncomment

--- Day 5/builtin.js
+++ Day 5/builtin.js
@@ -1,9 +1,9 @@
-/* console.log(Math.max(10, 19, 999, 129, 100, 120, 20. -1));
+ console.log(Math.max(10, 19, 999, 129, 100, 120, 20. -1));
 console.log(Math.abs(-999));
 console.log(Math.random());
 
 console.log(Math.floor(Math.random()*4));
- */
+
 
 console.log(Date.now());
 console.log(new Date().getDay());
